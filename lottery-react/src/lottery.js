9b7d9f2b08d5dcd45e2ef6c51eea6d38f5e9971e
jpmcruz <jason.paul.cruz@gmail.com>
2019-01-28 17:01:16 +0900

    added lottery.js, deployed lottery contract

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,54 @@
+import web3 from "./web3";
+
+const address = "0x65007596B749686C4F070C47d9c372ac85C559e4";
+
+//prettier
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  }
+];
+
+export default new web3.eth.Contract(abi, address); //this is our access to the contract in the blockchain
