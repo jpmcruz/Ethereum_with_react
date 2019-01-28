1a19b37c0129a9a158f40cb01cee42f2cf4eb516
jpmcruz <jason.paul.cruz@gmail.com>
2019-01-28 15:46:12 +0900

    added web3.js

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 class App extends Component {
   render() {
+    console.log(web3.version);
+      web3.eth.getAccounts()
+      .then(console.log);
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload jason.
           </p>
           <a
             className="App-link"
